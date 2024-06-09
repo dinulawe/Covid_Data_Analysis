d1b8c0aa8254e0571208ddac60e1b5be40e1763c
dinulawe <Nethan.w12@gmail.com>
2024-06-08 21:08:22 -0400

    Saved Changed Made

--- covid_data_analysis.sql
+++ covid_data_analysis.sql
@@ -47,7 +47,7 @@ Select location, MAX(cast(total_deaths as DECIMAL)) as TotalDeathCount
 From CovidData.coviddeaths
 -- Makes sure that the data returned does not have the total continent death count
 Where continent != ""
-Group by location
+Group by location 
 order by TotalDeathCount desc;
 
 
@@ -105,7 +105,33 @@ where dea.continent != "";
 Select *
 From PercentPopulationVaccinated;
 
+-- Queries for Tableau
 
+-- 1
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as decimal)) as total_deaths, SUM(cast(new_deaths as decimal))/SUM(New_Cases)*100 as DeathPercentage
+From CovidData.coviddeaths
+where continent != "" 
+order by 1,2;
+
+-- 2
+Select continent, SUM(cast(new_deaths as decimal)) as TotalDeathCount
+From CovidData.coviddeaths
+Where continent != "" 
+-- and continent not in ('World', 'European Union', 'International')
+Group by continent
+order by TotalDeathCount desc;
+
+-- 3
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidData.coviddeaths
+Group by Location, Population
+order by PercentPopulationInfected desc;
+
+-- 4
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidData.coviddeaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc
 
 
 
